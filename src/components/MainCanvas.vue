ceae85109762d1f733c712ce0ce8c58bc84e672d
Miško Dvořák <michal.dvorak999@gmail.com>
2022-04-12 11:37:18 +0200

    client app initializion

---
+++ src/components/MainCanvas.vue
@@ -0,0 +1,29 @@
+<style>
+canvas{
+  width: 800px;
+  height: 600px;
+  border: 1px solid lightblue;
+  border-radius: 3px;
+}
+</style>
+
+<template>
+  <canvas @click="canvasClick"></canvas>
+</template>
+
+<script>
+
+export default {
+  name: "Canvas",
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    canvasClick: () => {
+
+    }
+  }
+}
+</script>
